94e40bef998e310d316d0b9f53a11d087a4df4ec
neel <neelkb100@gmail.com>
2020-07-06 03:29:55 +0530

    UserTab, ProfileTab

--- app/src/main/java/com/example/instagramc/ProfileTab.java
+++ app/src/main/java/com/example/instagramc/ProfileTab.java
@@ -1,5 +1,6 @@
 package com.example.instagramc;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,58 +8,118 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ProfileTab#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
+import com.shashank.sony.fancytoastlib.FancyToast;
+
+
 public class ProfileTab extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+  private EditText editTextPName,editTextPBio,editTextPProfession,editTextPHobbies,editTextPFavSports;
+  private Button buttonSaveInfo;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public ProfileTab() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ProfileTab.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ProfileTab newInstance(String param1, String param2) {
-        ProfileTab fragment = new ProfileTab();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    final ParseUser parseUser = ParseUser.getCurrentUser();
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile_tab, container, false);
+
+
+        View view = inflater.inflate(R.layout.fragment_profile_tab, container, false);
+
+        editTextPName = view.findViewById(R.id.edtProfileName);
+        editTextPBio = view.findViewById(R.id.edtProfileBio);
+        editTextPProfession = view.findViewById(R.id.edtProfileProfession);
+        editTextPHobbies = view.findViewById(R.id.edtProfileHobbies);
+        editTextPFavSports = view.findViewById(R.id.edtProfileFavSports);
+
+        buttonSaveInfo  = view.findViewById(R.id.btnUpdateInfo);
+
+        profileDataCheckAndSet();
+
+        buttonSaveInfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                parseUser.put("profileName",editTextPName.getText().toString());
+                parseUser.put("profileBio",editTextPBio.getText().toString());
+                parseUser.put("profileProfession",editTextPProfession.getText().toString());
+                parseUser.put("profileHobbies",editTextPHobbies.getText().toString());
+                parseUser.put("profileFavSport",editTextPFavSports.getText().toString());
+
+                final ProgressDialog progressDialog = new ProgressDialog(getContext());
+
+                parseUser.saveInBackground(new SaveCallback() {
+                    @Override
+                    public void done(ParseException e) {
+
+                        if(e == null ){
+
+                            progressDialog.setMessage("Saving data in progress");
+                            progressDialog.show();
+
+                            FancyToast.makeText(getContext(),"Info Save Successfully",FancyToast.LENGTH_SHORT,FancyToast.INFO,false).show();
+                        }
+                        else {
+
+                            FancyToast.makeText(getContext(),e.getMessage(),FancyToast.LENGTH_SHORT,FancyToast.ERROR,false).show();
+                        }
+
+                        progressDialog.dismiss();
+                    }
+                });
+            }
+        });
+
+        return view;
+    }
+
+
+    public void profileDataCheckAndSet(){
+
+        if(parseUser.get("profileName") == null){
+
+            editTextPName.setText("");
+        }
+        else {
+            editTextPName.setText(parseUser.get("profileName").toString());
+        }
+
+
+        if(parseUser.get("profileBio") == null){
+
+            editTextPBio.setText("");
+        }
+        else {
+            editTextPBio.setText(parseUser.get("profileBio").toString());
+        }
+
+        if(parseUser.get("profileProfession") == null){
+
+            editTextPProfession.setText("");
+        }
+        else {
+            editTextPProfession.setText(parseUser.get("profileProfession").toString());
+        }
+
+        if(parseUser.get("profileHobbies") == null){
+
+            editTextPHobbies.setText("");
+        }
+        else {
+            editTextPHobbies.setText(parseUser.get("profileHobbies").toString());
+        }
+
+        if(parseUser.get("profileFavSport") == null){
+
+            editTextPFavSports.setText("");
+        }
+        else {
+            editTextPFavSports.setText(parseUser.get("profileFavSport").toString());
+        }
     }
 }
--- app/src/main/java/com/example/instagramc/UsersTab.java
+++ app/src/main/java/com/example/instagramc/UsersTab.java
@@ -7,16 +7,67 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class UsersTab extends Fragment {
 
-
+    private ListView listViewUsersList;
+    private View view;
+    private ArrayList arrayList;
+    private ArrayAdapter arrayAdapter;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_users_tab, container, false);
+
+
+        view = inflater.inflate(R.layout.fragment_users_tab, container, false);
+
+        listViewUsersList = view.findViewById(R.id.userListView);
+
+        arrayList = new ArrayList();
+
+        arrayAdapter = new ArrayAdapter(getContext(), android.R.layout.simple_list_item_1,arrayList);
+
+        final TextView textViewLoading = view.findViewById(R.id.loadingTxtView);
+
+        ParseQuery<ParseUser> parseQuery = ParseUser.getQuery();
+        parseQuery.whereNotEqualTo("username",ParseUser.getCurrentUser().getUsername());
+        parseQuery.findInBackground(new FindCallback<ParseUser>() {
+            @Override
+            public void done(List<ParseUser> users, ParseException e) {
+
+                if(e == null){
+
+                    if(users.size() >0){
+
+                        for (ParseUser user : users){
+
+                            arrayList.add(user.getUsername());
+
+                        }
+
+                        listViewUsersList.setAdapter(arrayAdapter);
+                        textViewLoading.animate().alpha(0).setDuration(2000);
+                        listViewUsersList.setVisibility(View.VISIBLE);
+
+                    }
+                }
+            }
+        });
+
+
+        return view ;
     }
 }
